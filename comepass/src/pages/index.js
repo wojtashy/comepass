8712a63b790f10324b8b5fe3b3703b66e7e4e83c
Wojtek <wojtek01259@gmail.com>
2022-07-12 00:18:29 +0200

    added content to the landing page

--- comepass/src/pages/index.js
+++ comepass/src/pages/index.js
@@ -1,12 +1,37 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
+import { StaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import backgroundVideo from '../videos/bg-video-reason.mp4'
 
 const utmParameters = `?utm_source=starter&utm_medium=start-page&utm_campaign=default-starter`
+
+const AboutUs = () =>{
+
+  return(
+    <div className="about-section">
+      <h1 className="about-section-title">About</h1>
+      <StaticImage src='../images/comepass_white.png' alt='comepass' className="about-comepass_image"/>
+      <StaticImage src='../images/comepass_band.png' alt='comepass band' className="about-comepass-band_image"/>
+      <StaticQuery query={graphql`query  {
+  Comepass {
+    bandInfo(where: {id: "cl5h782h585qe0cuif9e5kyie"}) {
+      bandDescription
+    }
+  }
+}`} render={data=>(
+  <div className="about_comepass-description">
+    <p>
+      {data.Comepass.bandInfo.bandDescription}
+    </p>
+  </div>
+)}/>
+    </div>
+  )
+
+}
 
 const IndexPage = () => (
   <Layout>
@@ -29,7 +54,7 @@ const IndexPage = () => (
         </div>
 
     </div>
-    Siema siema
+    <AboutUs/>
   </Layout>
 )
 
