d7d86e412a3c3b11a28ca1fecc80fc7f25686c28
Wojtek <wojtek01259@gmail.com>
2022-07-21 01:10:30 +0200

    began working on music page

--- comepass/src/pages/music.js
+++ comepass/src/pages/music.js
@@ -1,12 +1,47 @@
 import * as React from "react"
-
+import { StaticQuery,graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+
+const MusicBar = () => {
+  return(
+    <div>
+      <StaticQuery
+        query={graphql`query {
+  Comepass {
+    bandtracks {
+      lyrics
+      songTitle
+      trackLyrics {
+        html
+      }
+      trackImage{
+        url
+      }
+    }
+  }
+}
+
+        `}render={data=>(data.Comepass.bandtracks.map(track=>(
+          <>
+          <h2>{track.songTitle}</h2>
+            <code>
+            {track.trackLyrics.html}
+            </code>
+          <img src={track.trackImage.url} alt='comepass song'/>
+          {console.log(data)}
+          </>
+          
+    )))}
+       ></StaticQuery>
+    </div>
+  )
+  } 
 
 const MusicPage = () => (
   <Layout>
     <Seo title="Comepass music" />
-    To jest nasza strona z muzyka
+    <MusicBar></MusicBar>
   </Layout>
 )
 
