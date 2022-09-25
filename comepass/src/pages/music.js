fd704523a7b4201db5ebad8595fa3bfaf5d53380
Wojtek <wojtek01259@gmail.com>
2022-09-26 01:10:14 +0200

    improved graphql querry + redesign

--- comepass/src/pages/music.js
+++ comepass/src/pages/music.js
@@ -2,79 +2,66 @@ import * as React from "react"
 import { StaticQuery,graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { StaticImage } from "gatsby-plugin-image"
+import tidal from "../images/tidal-white.png"
+import spotify from "../images/white-spotify.png"
+import youtube from "../images/white-youtube.png"
 const MusicBar = () => {
    const [lyricsState,setLyricsState] = React.useState('hidden');
 
-   const handleLyricsView = ()=>{
+   const handleLyricsView = () =>{
     lyricsState == 'hidden' ? setLyricsState('visible') : setLyricsState('hidden')
    }
   
    return(
-    <div className="tracks__bar">
+    <>
+    <div className="tracks__container">
       <StaticQuery
         query={graphql`query {
-  Comepass {
-    bandtracks {
-      lyrics
-      songTitle
-      trackLyrics {
-        html
-      }
-      trackImage{
-        url
-      }
-    }
-  }
-}
-
-        `}render={data=>(data.Comepass.bandtracks.map(track=>(
+          Comepass {
+            bandtracks {
+              songTitle
+              spotifyLink
+              tidalLink
+              youtubeLink
+              trackImage {
+                url
+              }
+              trackLyrics { 
+                html
+              }
+            }
+          }
+        }        
+        `}render={data=>(data.Comepass.bandtracks.map(track,i=>(
           <div className="tracks_bar-track">
           <h2 className="tracks_bar-track-title">{track.songTitle}</h2> 
-          <img src={track.trackImage.url} className="tracks_bar-track-image" alt='comepass song'/>
-          <div className={`track_bar-sm-links`}>
-                
-                        <a href='https://tidal.com/browse/artist/28465999' target="_blank" rel="noopener noreferrer" className="track_bar_links--item">
-                            <StaticImage alt='tidal'  src='../images/tidal-white.png'/>
-                        </a> 
-                        <a href='https://open.spotify.com/artist/5Ta2BXVfYW1o06F9zrCGjR' className="track_bar_links--item">
-                            <StaticImage alt='spotify'  src='../images/white-spotify.png'/>
-                        </a>
-                        <a href='https://www.instagram.com/comepass_official' className="track_bar_links--item">
-                            <StaticImage alt='instagram'  src='../images/white-instagram.png'/>
-                        </a>
-                        <a href='https://www.facebook.com/comepassband' className="track_bar_links--item">
-                            <StaticImage alt='facebook'  src='../images/facebook-white.png'/>
-                        </a>
-                        <a href='https://www.youtube.com/c/Comepass' className="track_bar_links--item">
-                            <StaticImage alt='youtube'  src='../images/white-youtube.png'/>
-                        </a>
-
-                
+          <div className={`track__bar--smLinks`}>
+                <a href={track.spotifyLink}>
+                  <img src={spotify} className={`track__card--smIcons`}/>
+                </a>
+                <a href={track.youtubeLink}>
+                  <img src={youtube} className={`track__card--smIcons`}/>
+                </a>
+                <a href={track.tidalLink}>
+                  <img src={tidal} className={`track__card--smIcons`}/>
+                </a>
             </div>
-          <div onClick={handleLyricsView} className={`tracks_bar-lyrics-${lyricsState} tracks_bar-lyrics`} dangerouslySetInnerHTML={{__html: `${track.trackLyrics.html}`}} ></div> 
+            <img src={track.trackImage.url} className="tracks_bar-track-image" alt='comepass song'/>
+          <div onClick={handleLyricsView(i)} className={`tracks_bar-lyrics-${lyricsState} tracks_bar-lyrics`} dangerouslySetInnerHTML={{__html: `${track.trackLyrics.html}`}} ></div> 
 
           </div>
           
     )))}
        ></StaticQuery>
     </div>
+    </>
   )
   } 
-
-  const SocialMediaIcons = () =>{
-
-    return(
-        <>
-            
-        </>
-    )
-
-}
 
 const MusicPage = () => (
   <Layout>
     <Seo title="Comepass music" />
+    <h1 className="music__title">Twórczość</h1>
     <MusicBar></MusicBar>
   </Layout>
 )
